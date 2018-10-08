b28aedc753ed2ed49d33dc4ec48e2f657253e4b9
Fernando <fernandovieiraf02@gmail.com>
2018-10-08 19:42:03 -0400

    Adicionado a opção de logout

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ App created with React Native, Redux and Firebase.
 
 //Para iniciar o projeto após clonagem, executar os comandos:
 npm install
+
 //Para executar em dispositivos Androids
-react-native run-android
+react-native run-android    
+
 //Para executar em dispositivos IOS
 react-native run-ios
 
--- src/components/DrawerNavigatorContent.js
+++ src/components/DrawerNavigatorContent.js
@@ -5,6 +5,7 @@ import Styles from '../utils/Styles';
 import Colors from '../utils/Colors';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { DrawerItems } from 'react-navigation';
+import firebase from 'firebase';
 
 
 const EdgeDivisor = () => (
@@ -14,6 +15,14 @@ const EdgeDivisor = () => (
 const UserIconDefault = () => (
     <Image source={require('../assets/user_default_icon.png')} style={{height: 100, width: 100, resizeMode: 'contain'}}/>
 );
+
+const LogOut = (props) => {
+    firebase.auth().signOut()
+    .then(() => {
+        props.navigation.navigate('InitialRoutes');
+    })
+    .catch(err => console.log("Erro ao encerrar a sessão!"));
+}
 
 const DrawerNavigatorContent = (props) => (
   <Container style={{ flex: 1 }}>
@@ -32,6 +41,9 @@ const DrawerNavigatorContent = (props) => (
     <View style={ Styles.DrawerNavigatorFooter }>
         <Text style={ Styles.DrawerNavigatorLabelText }>Configurações</Text>
         <Text style={ Styles.DrawerNavigatorLabelText }>Avalie-nos</Text>
+        <TouchableOpacity onPress={ () => LogOut(props)}>
+            <Text style={ Styles.DrawerNavigatorLabelText }>Sair</Text>
+        </TouchableOpacity>
     </View>
   </Container>
 );
